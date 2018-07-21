4341cff583b690581bd6af42fde7a88d8128f5d9
Takahiro Kobayashi <koba925@gmail.com>
2018-07-21 13:52:56 +0900

    ALDS1_2_C: Solved

--- ALDS1_2_C.py
+++ ALDS1_2_C.py
@@ -6,15 +6,17 @@ def swap(c, i, j):
     c[i] = c[j]
     c[j] = t
 
-def bubble_sort(c, f):
+def bubble_sort(cs, f):
+    c = cs[:]
     for i in range(len(c) - 1):
-        for j in range(i + 1, len(c)):
-            # print(i, j)
-            if f(c[i]) > f(c[j]):
-                swap(c, i, j)
+        for j in range(len(c) - 1, i, -1):
+            if f(c[j - 1]) > f(c[j]):
+                swap(c, j - 1, j)
+            # print(i, j, c)
     return c
 
-def insertion_sort(c, f):
+def insertion_sort(cs, f):
+    c = cs[:]
     for i in range(1, len(c)):
         w = c[i]
         j = i - 1
@@ -24,16 +26,52 @@ def insertion_sort(c, f):
         c[j + 1] = w
     return c
 
-def selection_sort(c, f):
-    for i in range(len(c) - 1):
+def selection_sort(cs, f):
+    c = cs[:]
+    for i in range(len(c)):
         minj = i
-        for j in range(i + 1, len(c)):
+        for j in range(i, len(c)):
             if f(c[j]) < f(c[minj]):
                 minj = j
         swap(c, i, minj)
+        # print(c)
     return c
 
-#def is_stable(c, s): 
+def to_group(c):
+    b = {}
+    for i in c:
+        if not(i[1] in b):
+            b[i[1]] = [i]
+        else:
+            b[i[1]] = b[i[1]] + [i]
+    return b
+
+def is_stable(c, s): 
+    bc = to_group(c)
+    bs = to_group(s(c, lambda x: x[1]))
+    # print(bc)
+    # print(bs)
+    for k in bc:
+        if bc[k] != bs[k]:
+            return "Not stable"
+    return "Stable"
+
+n = int(input())
+c = input().split()
+
+print(" ".join(bubble_sort(c, lambda x: x[1])))
+print(is_stable(c, bubble_sort))
+
+print(" ".join(selection_sort(c, lambda x: x[1])))
+print(is_stable(c, selection_sort))
+
+exit()
+
+print(is_stable(['C4', 'H3', 'S3', 'C1', 'D2'], bubble_sort))
+print(is_stable(['C4', 'H3', 'S3', 'C1', 'D2'], insertion_sort))
+print(is_stable(['C4', 'H3', 'S3', 'C1', 'D2'], selection_sort))
+
+print(bubble_sort(['C4', 'H3', 'S3', 'C1', 'D2'], lambda x: x[1]))
 
 print(bubble_sort(['C4', 'H3', 'S3', 'C1', 'D2'], lambda x: x[1]))
 print(bubble_sort(['H3', 'S3', 'D2', 'C4', 'C1'], lambda x: x[1]))
@@ -46,5 +84,3 @@ print(insertion_sort(['C1', 'C4', 'D2', 'H3', 'S3'], lambda x: x[1]))
 print(selection_sort(['C4', 'H3', 'S3', 'C1', 'D2'], lambda x: x[1]))
 print(selection_sort(['H3', 'S3', 'D2', 'C4', 'C1'], lambda x: x[1]))
 print(selection_sort(['C1', 'C4', 'D2', 'H3', 'S3'], lambda x: x[1]))
-
-
