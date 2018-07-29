2a11c362a6a1fd869797eae6519ff6c5b3a6001e
Takahiro Kobayashi <koba925@gmail.com>
2018-07-29 15:54:35 +0900

    ALDS1_3_C: Introduced Sentinel

--- ALDS1_3_C.py
+++ ALDS1_3_C.py
@@ -4,82 +4,114 @@
 class DoublyLinkedNode:
 
     def __init__(self, x):
+        self.value = x
         self.prev = None
         self.next = None
-        self.value = x
 
-    def print(self):
-        print(self.value, end = "")
+    def str(self):
+        return str(self.value)
 
 class DoublyLinkedList:
 
-    def __init__(self, n):
-        self.head = None
-        self.tail = None
+    def __init__(self):
+        self.nil = DoublyLinkedNode(None)
+        self.nil.prev = self.nil.next = self.nil
 
     def insert(self, x):
         node = DoublyLinkedNode(x)
-        node.prev = None
-        node.next = self.head
-        if self.head is None:
-            self.tail = node
-        else:
-            self.head.prev = node
-        self.head = node
+        node.prev = self.nil
+        node.next = self.nil.next
+        self.nil.next.prev = node
+        self.nil.next = node
+
+    def search(self, x):
+        node = self.nil.next
+        while node is not self.nil and node.value != x:
+            node = node.next
+        return node
+    
+    def delete_node(self, node):
+        # print("delete_node:", node.value, node.prev.value, node.next.value)
+        if node is self.nil:
+            return
+        next = node.next
+        node.prev.next = next
+        next.prev = node.prev
 
     def delete(self, x):
-        node = self.head
-        while node is not None:
-            if node.value == x:
-                break
-            node = node.next
-        else:
-            return
-
-        if node is self.head and node is self.tail:
-            self.head = None
-            self.tail = None
-        elif node is self.head:
-            self.head = node.next
-            node.next.prev = None
-        elif node is self.tail:
-            self.tail = node.prev
-            node.prev.next = None
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
+        self.delete_node(self.search(x))
 
     def delete_first(self):
-        self.head = self.head.next
-        if self.head is None:
-            self.tail = None
-        else:
-            self.head.prev = None
+        self.delete_node(self.nil.next)
 
     def delete_last(self):
-        self.tail = self.tail.prev
-        if self.tail is None:
-            self.head = None
-        else:
-            self.tail.next = None
+        self.delete_node(self.nil.prev)
 
-    def print(self):
-        node = self.head
-        while node is not None:
-            node.print()
+    def str(self):
+        node = self.nil.next
+        v = ""
+        while node is not self.nil:
+            v += node.str()
             node = node.next
-            if node is not None:
-                print(" ", end = "")
-        print()
+            if node is not self.nil:
+                v += " "
+        return v
 
-def _main():
+    def debug_str(self):
+        v = self.nil.next.str() + " "
+        v += self.nil.prev.str() + " | "
+        v += self.str()
+        return v
+
+def test():
+    dll = DoublyLinkedList()
+    assert dll.debug_str() == "None None | "
+    dll.insert(1)
+    assert dll.debug_str() == "1 1 | 1"
+    dll.insert(2)
+    assert dll.debug_str() == "2 1 | 2 1"
+    dll.insert(3)
+    assert dll.debug_str() == "3 1 | 3 2 1"
+    dll.delete(2)
+    assert dll.debug_str() == "3 1 | 3 1"
+    dll.delete(1)
+    assert dll.debug_str() == "3 3 | 3"
+    dll.delete(3)
+    assert dll.debug_str() == "None None | "
+    dll.insert(1)
+    assert dll.debug_str() == "1 1 | 1"
+    dll.insert(2)
+    assert dll.debug_str() == "2 1 | 2 1"
+    dll.delete(2)
+    assert dll.debug_str() == "1 1 | 1"
+    dll.delete(1)
+    assert dll.debug_str() == "None None | "
+    dll.insert(1)
+    assert dll.debug_str() == "1 1 | 1"
+    dll.insert(2)
+    assert dll.debug_str() == "2 1 | 2 1"
+    dll.delete_first()
+    assert dll.debug_str() == "1 1 | 1"
+    dll.delete_first()
+    assert dll.debug_str() == "None None | "
+    dll.insert(1)
+    assert dll.debug_str() == "1 1 | 1"
+    dll.insert(2)
+    assert dll.debug_str() == "2 1 | 2 1"
+    dll.delete_last()
+    assert dll.debug_str() == "2 2 | 2"
+    dll.delete_last()
+    assert dll.debug_str() == "None None | "
+
+def main():
     n = int(input())
-    dll = DoublyLinkedList(n)
+    dll = DoublyLinkedList()
+    # print(dll.debug_str())
 
-    import sys
+    from sys import stdin
 
-    for l in sys.stdin:
-        line = l.split()
+    for _ in range(n):
+        line = stdin.readline().split()
         if line[0] == "insert":
             dll.insert(int(line[1]))
         elif line[0] == "delete":
@@ -88,8 +120,10 @@ def _main():
             dll.delete_first()
         elif line[0] == "deleteLast":
             dll.delete_last()
+        # print(dll.debug_str())
 
-    dll.print()
+    print(dll.str())
 
-if __name__ == "__main__":
-    _main()
+test()
+
+main()
