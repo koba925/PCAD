f69a0749051d9c230768cc3cfa9adc86119f9d5d
Takahiro Kobayashi <koba925@gmail.com>
2018-08-13 07:57:51 +0900

    ALDS1_5_A: Make internal function

--- ALDS1_5_A.py
+++ ALDS1_5_A.py
@@ -1,26 +1,27 @@
 #! /usr/local/bin/python3
 # coding: utf-8
 
-def can_make_sum(A, k, m):
-    #print(A, k, m)
-    if m == 0:
-        return True
-    elif m < 0 or k == len(A):
-        return False
-    else: 
-        return (can_make_sum(A, k + 1, m - A[k]) or
-                can_make_sum(A, k + 1, m))
-
 def solve():
-    _ = int(input())
+
+    def can_make_sum(k, m):
+        #print(A, k, m)
+        if m == 0:
+            return True
+        elif m < 0 or k == n:
+            return False
+        else: 
+            return (can_make_sum(k + 1, m - A[k]) or
+                    can_make_sum(k + 1, m))
+
+    n = int(input())
     A = [int(x) for x in input().split()]
     _ = int(input())
     M = [int(x) for x in input().split()]
 
     for m in M:
-        print("yes" if can_make_sum(A, 0, m) else "no")
+        print("yes" if can_make_sum(0, m) else "no")
 
-def test():
+""" def test():
     assert can_make_sum([1, 3, 7], 0, 1)
     assert can_make_sum([1, 3, 7], 0, 3)
     assert can_make_sum([1, 3, 7], 0, 7)
@@ -31,7 +32,7 @@ def test():
     assert not can_make_sum([1, 3, 7], 0, 2)
     assert not can_make_sum([1, 3, 7], 0, 5)
     assert not can_make_sum([1, 3, 7], 0, 12)
-
+"""
 # test()
 # exit()
 
