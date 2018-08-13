2e36ec95144714e436df3e680d27bb562ee7892f
Takahiro Kobayashi <koba925@gmail.com>
2018-08-13 10:53:58 +0900

    ALDS1_5_A: Memoize and AC

--- ALDS1_5_A.py
+++ ALDS1_5_A.py
@@ -2,17 +2,21 @@
 # coding: utf-8
 
 def solve():
-
     def can_make_sum(k, m):
-        #print(A, k, m)
-        if m == 0:
-            return True
-        elif m < 0 or k == n:
-            return False
-        else: 
-            return (can_make_sum(k + 1, m - A[k]) or
-                    can_make_sum(k + 1, m))
+        def cmi(k, m):
+            if m == 0:
+                return True
+            elif m < 0 or k == n:
+                return False
+            else: 
+                return (can_make_sum(k + 1, m - A[k]) or
+                        can_make_sum(k + 1, m))
 
+        if (k, m) not in cache:
+            cache[(k, m)] = cmi(k, m)
+        return cache[(k, m)]
+
+    cache = {}
     n = int(input())
     A = [int(x) for x in input().split()]
     _ = int(input())
@@ -20,20 +24,5 @@ def solve():
 
     for m in M:
         print("yes" if can_make_sum(0, m) else "no")
-
-""" def test():
-    assert can_make_sum([1, 3, 7], 0, 1)
-    assert can_make_sum([1, 3, 7], 0, 3)
-    assert can_make_sum([1, 3, 7], 0, 7)
-    assert can_make_sum([1, 3, 7], 0, 4)
-    assert can_make_sum([1, 3, 7], 0, 8)
-    assert can_make_sum([1, 3, 7], 0, 10)
-    assert can_make_sum([1, 3, 7], 0, 11)
-    assert not can_make_sum([1, 3, 7], 0, 2)
-    assert not can_make_sum([1, 3, 7], 0, 5)
-    assert not can_make_sum([1, 3, 7], 0, 12)
-"""
-# test()
-# exit()
 
 solve()
