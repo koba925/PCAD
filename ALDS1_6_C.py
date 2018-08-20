1274f86605c160ef848e1b5a265d8f731610ff61
Takahiro Kobayashi <koba925@gmail.com>
2018-08-20 23:09:07 +0900

    ALDS1_6_C.py: PEP-8 Style

--- ALDS1_6_C.py
+++ ALDS1_6_C.py
@@ -4,11 +4,16 @@
 from sys import stdin
 from math import inf
 
+
 def merge(A, left, mid, right):
-    L = A[left:mid]; L.append(("", inf))
-    R = A[mid:right]; R.append(("", inf))
-    i = iter(L); l = next(i)
-    j = iter(R); r = next(j)
+    L = A[left:mid]
+    L.append(("", inf))
+    R = A[mid:right]
+    R.append(("", inf))
+    i = iter(L)
+    l = next(i)
+    j = iter(R)
+    r = next(j)
     for k in range(left, right):
         if l[1] <= r[1]:
             A[k] = l
@@ -17,17 +22,20 @@ def merge(A, left, mid, right):
             A[k] = r
             r = next(j)
 
+
 def merge_sort(A, left, right):
     if left + 1 < right:
         mid = (left + right) // 2
         merge_sort(A, left, mid)
         merge_sort(A, mid, right)
         merge(A, left, mid, right)
-    
+
+
 def swap(A, i, j):
     tmp = A[i]
     A[i] = A[j]
     A[j] = tmp
+
 
 def partition(A, p, r):
     x = A[r][1]
@@ -39,11 +47,13 @@ def partition(A, p, r):
     swap(A, i + 1, r)
     return i + 1
 
+
 def quick_sort(A, p, r):
     if p < r:
         q = partition(A, p, r)
         quick_sort(A, p, q - 1)
         quick_sort(A, q + 1, r)
+
 
 def main():
     n = int(stdin.readline())
