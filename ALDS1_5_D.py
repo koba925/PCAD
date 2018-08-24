143f5e15de2825f154ab02a9fbb305771289d667
Takahiro Kobayashi <koba925@gmail.com>
2018-08-25 08:21:51 +0900

    ALDS1_5_D: Quicksort Version (WA)

--- ALDS1_5_D.py
+++ ALDS1_5_D.py
@@ -1,24 +1,44 @@
 #! /usr/local/bin/python3
 # coding: utf-8
 
-from bisect import bisect_left
+from sys import stdin
+
+cnt = 0
 
 
-def count_inversion(A):
-    cnt = 0
-    for i in reversed(range(len(A) - 1)):
-        j = bisect_left(A, A[i], i + 1, len(A))
-        cnt += j - i - 1
-        tmp = A[i]
-        del A[i]
-        A.insert(j - 1, tmp)
-    return cnt
+def swap(A, i, j):
+    global cnt
+
+    cnt += j - i
+    tmp = A[i]
+    A[i] = A[j]
+    A[j] = tmp
+    print(*A, i, j)
+
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            swap(A, i, j)
+    swap(A, i + 1, r)
+    return i + 1
+
+
+def count_inversion(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        count_inversion(A, p, q - 1)
+        count_inversion(A, q + 1, r)
 
 
 def main():
     n = int(input())
     A = [int(x) for x in input().split()]
-    print(count_inversion(A))
+    count_inversion(A, 0, n - 1)
+    print(cnt)
 
 
 main()
