3d7a327f6c75eb2ba4861fd289a0eec4ea29b70b
Takahiro Kobayashi <koba925@gmail.com>
2018-08-26 20:48:28 +0900

    ALDS1_5_D: Solved

--- ALDS1_5_D.py
+++ ALDS1_5_D.py
@@ -1,44 +1,42 @@
 #! /usr/local/bin/python3
 # coding: utf-8
 
-from sys import stdin
+from math import inf
 
-cnt = 0
-
-
-def swap(A, i, j):
-    global cnt
-
-    cnt += j - i
-    tmp = A[i]
-    A[i] = A[j]
-    A[j] = tmp
-    print(*A, i, j)
+inv = 0
 
 
-def partition(A, p, r):
-    x = A[r]
-    i = p - 1
-    for j in range(p, r):
-        if A[j] <= x:
+def merge(A, left, mid, right):
+    global inv
+
+    L = A[left:mid]
+    L.append(inf)
+    R = A[mid:right]
+    R.append(inf)
+    i = j = 0
+    for k in range(left, right):
+        if L[i] < R[j]:
+            A[k] = L[i]
             i += 1
-            swap(A, i, j)
-    swap(A, i + 1, r)
-    return i + 1
+        else:
+            inv += len(L) - i - 1
+            A[k] = R[j]
+            j += 1
 
 
-def count_inversion(A, p, r):
-    if p < r:
-        q = partition(A, p, r)
-        count_inversion(A, p, q - 1)
-        count_inversion(A, q + 1, r)
+def merge_sort(A, left, right):
+    if left + 1 < right:
+        mid = (left + right) // 2
+        merge_sort(A, left, mid)
+        merge_sort(A, mid, right)
+        merge(A, left, mid, right)
 
 
 def main():
     n = int(input())
     A = [int(x) for x in input().split()]
-    count_inversion(A, 0, n - 1)
-    print(cnt)
+    merge_sort(A, 0, n)
+    print(inv)
 
 
 main()
