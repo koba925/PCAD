7577df49bd20e13ebc296609a35f43718d500624
Takahiro Kobayashi <koba925@gmail.com>
2018-09-22 22:09:05 +0900

    ALDS1_9_C: PriorityQueue Class

--- ALDS1_9_C.py
+++ ALDS1_9_C.py
@@ -10,171 +10,134 @@ def swap(A, i, j):
     A[j] = tmp
 
 
-def null_heap():
-    return [None]
+class NoMoreDataError(Exception):
+    pass
 
 
-def parent_node(k):
-    return k // 2
+class PriorityQueue():
+    def __init__(self, elems=[]):
+        self.A = [None] + elems
+        self._build_max_heap()
 
+    def __repr__(self):
+        return str(self.A[1:])
 
-def left_node(k):
-    return 2 * k
+    def is_empty(self):
+        return len(self.A) == 1
 
+    def insert(self, k):
+        self.A.append(k)
 
-def right_node(k):
-    return 2 * k + 1
-
-
-def sibling_node(k):
-    assert k > 1
-
-    parent = parent_node(k)
-    if is_left(k):
-        return right_node(parent)
-    else:
-        return left_node(parent)
-
-
-def last_node(S):
-    return len(S) - 1
-
-
-def is_empty(S):
-    return len(S) == 1
-
-
-def is_left(i):
-    return i % 2 == 0
-
-
-def insert(S, k):
-
-    def rec(i):
-        nonlocal S
-
+        i = self._last()
         if i == 1:
             return
 
-        p = parent_node(i)
-        if S[p] < S[i]:
-            swap(S, p, i)
-            rec(p)
+        while i > 1:
+            p = self._parent(i)
+            if self.A[p] >= self.A[i]:
+                break
+            swap(self.A, p, i)
+            i = p
 
-    S.append(k)
-    rec(last_node(S))
-
-
-def max_heapify(S, i):
-    l = left_node(i)
-    r = right_node(i)
-    if l <= last_node(S) and S[l] > S[i]:
-        largest = l
-    else:
-        largest = i
-    if r <= last_node(S) and S[r] > S[largest]:
-        largest = r
-
-    if largest != i:
-        swap(S, i, largest)
-        max_heapify(S, largest)
-
-
-def extract_max(S):
-    if last_node(S) == 1:
-        return S.pop()
-    else:
-        k = S[1]
-        S[1] = S.pop()
-        max_heapify(S, 1)
-        return k
-
-
-def extract_max_old(S):
-
-    def rec(i, k):
-        nonlocal S
-
-        if i == 1:
-            tmp = S[i]
-            S[i] = k
-            return tmp
-
-        parent = parent_node(i)
-        sibling = sibling_node(i)
-        larger = i if S[i] > S[sibling] else sibling
-        tmp = S[larger]
-        S[larger] = k
-        return rec(parent, tmp)
-
-    last = last_node(S)
-    if last == 1:
-        return S.pop()
-
-    parent = parent_node(last)
-    if is_left(last):
-        return rec(parent, S.pop())
-    else:
-        left = left_node(parent)
-        if S[left] > S[last]:
-            v = S[left]
-            S[left] = S.pop()
-            return rec(parent, v)
+    def extract_max(self):
+        if self._last() == 0:
+            raise NoMoreDataError
+        if self._last() == 1:
+            return self.A.pop()
         else:
-            return rec(parent, S.pop())
+            k = self.A[1]
+            self.A[1] = self.A.pop()
+            self._max_heapify(1)
+            return k
 
+    def to_array(self):
+        R = []
+        while not self.is_empty():
+            R.append(self.extract_max())
+        return R
 
-def print_heap(A):
-    for i in range(1, last_node(A) + 1):
-        print(" {}".format(A[i]), end="")
-    print()
+    def _parent(self, k):
+        return k // 2
+
+    def _left(self, k):
+        return 2 * k
+
+    def _right(self, k):
+        return 2 * k + 1
+
+    def _last(self):
+        return len(self.A) - 1
+
+    def _max_heapify(self, i):
+        l = self._left(i)
+        r = self._right(i)
+        if l <= self._last() and \
+           self.A[l] > self.A[i]:
+            largest = l
+        else:
+            largest = i
+        if r <= self._last() and self.A[r] > self.A[largest]:
+            largest = r
+
+        if largest != i:
+            swap(self.A, i, largest)
+            self._max_heapify(largest)
+
+    def _build_max_heap(self):
+        for i in reversed(range(1, self._last() // 2 + 1)):
+            self._max_heapify(i)
 
 
 from random import randrange
 
 
-def random_array(n):
-    A = list(range(n))
+def test_a_case_1(A):
+    Q = PriorityQueue(A)
+    R = Q.to_array()
+    assert list(reversed(sorted(A))) == R, \
+        "\nA={}\nR={}".format(A, R)
+
+
+def test_a_case_2(A):
+    Q = PriorityQueue([])
+    for n in A:
+        Q.insert(n)
+    R = Q.to_array()
+    assert list(reversed(sorted(A))) == R, \
+        "\nA={}\nR={}".format(A, R)
+
+
+def random_array(max, n):
+    A = [randrange(max) for _ in range(n)]
     for i in range(n):
         swap(A, i, randrange(n))
     return A
 
 
-def from_array(S, A):
-    for k in A:
-        insert(S, k)
+def test_a_random_case_1():
+    test_a_case_1(random_array(15, 10))
 
 
-def from_heap(S):
-    R = []
-    while not is_empty(S):
-        R.append(extract_max(S))
-    return R
-
-
-def test_a_case():
-    A = random_array(15)
-    S = null_heap()
-    from_array(S, A)
-    R = from_heap(S)
-    assert list(reversed(sorted(A))) == R, \
-        "\nA={}\nR={}".format(A, R)
+def test_a_random_case_2():
+    test_a_case_2(random_array(15, 10))
 
 
 def test():
     for i in range(10000):
-        test_a_case()
+        test_a_random_case_1()
+        test_a_random_case_2()
 
 
 def main():
-    S = null_heap()
+    Q = PriorityQueue()
+    lines = stdin.readlines()
 
-    for line in stdin:
+    for line in lines:
         cmd = line.split()
         if cmd[0] == "insert":
-            insert(S, int(cmd[1]))
+            Q.insert(int(cmd[1]))
         elif cmd[0] == "extract":
-            print(extract_max(S))
+            print(Q.extract_max())
         else:
             break
 
