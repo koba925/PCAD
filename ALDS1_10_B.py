3d61ef419512676674da99740c99a9a96314c215
Takahiro Kobayashi <koba925@gmail.com>
2018-10-04 19:07:46 +0900

    ALDS1_10_B: Along with Text

--- ALDS1_10_B.py
+++ ALDS1_10_B.py
@@ -4,15 +4,6 @@
 from sys import maxsize
 
 
-def count_2(m1, m2):
-    assert m1[1] == m2[0]
-    return m1[0] * m1[1] * m2[1]
-
-
-def result_2(m1, m2):
-    return (m1[0], m2[1])
-
-
 def irange(start, stop=None):
     if stop:
         return range(start, stop + 1)
@@ -20,40 +11,28 @@ def irange(start, stop=None):
         return range(start + 1)
 
 
-def count(m):
+def count(ms):
+    n = len(ms)
 
-    lm = len(m)
-    if lm == 1:
-        return m[0], 0
+    p = [0] * (n + 1)
+    for i in range(n):
+        p[i] = ms[i][0]
+        p[i + 1] = ms[i][1]
 
-    cache = [[None] * (lm + 1) for _ in irange(lm)]
-    for i in range(lm):
-        cache[1][i + 1] = (m[i], 0)
+    m = [[None] * (n + 1) for _ in irange(n)]
+    for i in irange(1, n):
+        m[i][i] = 0
+    for l in irange(2, n):
+        for i in irange(1, n - l + 1):
+            j = i + l - 1
+            m[i][j] = maxsize
+            for k in irange(i, j - 1):
+                m[i][j] = min(
+                    m[i][j],
+                    m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]
+                )
 
-    for n in irange(2, lm):
-        # print("n:", n)
-        for i in irange(1, lm - n + 1):
-            # print("i:", i)
-            min_count = maxsize
-            min_result = (0, 0)
-            for j in irange(1, n - 1):
-                m1, c1 = cache[j][i]
-                m2, c2 = cache[n - j][i + j]
-                # print("j:{} m1:{} c1:{} m2:{} c2:{}".format(
-                #     j, m1, c1, m2, c2))
-                m3, c3 = result_2(m1, m2), count_2(m1, m2)
-                c = c1 + c2 + c3
-                if c < min_count:
-                    min_count = c
-                    min_result = m3
-            cache[n][i] = (min_result, min_count)
-
-    return min_result, min_count
-
-
-# print(count([(2, 3), (3, 3), (3, 2), (2, 4), (4, 1)]))
-# print(count([(30, 35), (35, 15), (15, 5), (5, 10), (10, 20), (20, 25)]))
-# exit()
+    return m[1][n]
 
 
 def main():
@@ -62,7 +41,7 @@ def main():
     m = []
     for _ in range(n):
         m.append(tuple(int(x) for x in input().split()))
-    _, c = count(m)
+    c = count(m)
     print(c)
 
 
