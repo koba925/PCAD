8b6162e1aea9cec10a1d0553bc3fc1fe2b6be79c
Takahiro Kobayashi <koba925@gmail.com>
2018-10-08 21:23:30 +0900

    ALDS1_11_D: Use set (RE)

--- ALDS1_11_D.py
+++ ALDS1_11_D.py
@@ -1,27 +1,31 @@
+from sys import setrecursionlimit
+
+
 def read_friends():
     n, m = [int(x) for x in input().split()]
-    adj_mat = [[False] * n for _ in range(n)]
+    adj_list = [set() for _ in range(n)]
 
     for _ in range(m):
         s, t = [int(x) for x in input().split()]
-        adj_mat[s][t] = True
-        adj_mat[t][s] = True
+        adj_list[s].add(t)
+        adj_list[t].add(s)
 
-    return n, adj_mat
+    return n, adj_list
 
 
-def divide_group(n, adj_mat):
+def divide_group(n, adj_list):
     group = [None] * n
 
-    def visit(k, g):
+    def visit(k, g, level):
+        print(level)
         group[k] = g
-        for t, connected in enumerate(adj_mat[k]):
-            if connected and group[t] is None:
-                visit(t, g)
+        for t in adj_list[k]:
+            if group[t] is None:
+                visit(t, g, level + 1)
 
     for k in range(n):
         if group[k] is None:
-            visit(k, k)
+            visit(k, k, 1)
 
     return group
 
@@ -35,10 +39,10 @@ def answer_queries(group):
 
 
 def main():
-    n, adj_mat = read_friends()
-    print(adj_mat)
-    group = divide_group(n, adj_mat)
-    print(group)
+    setrecursionlimit(10001)
+
+    n, adj_list = read_friends()
+    group = divide_group(n, adj_list)
     answer_queries(group)
 
 
