a6cd718c1c62d8a709e5144f98de05dd6d34f703
Takahiro Kobayashi <koba925@gmail.com>
2018-11-08 19:47:37 +0900

    DSL_2_C: my final version

--- DSL_2_C.py
+++ DSL_2_C.py
@@ -35,77 +35,69 @@ def read_points():
 
 
 def make_kd_tree(P):
-    T = [Node() for _ in range(len(P))]
-    np = 0
 
     def rec(l, r, depth):
-        nonlocal np
-
         if l >= r:
             return None
 
         mid = (l + r) // 2
-        t = np
-        np += 1
 
         if depth % 2 == 0:
             P[l:r] = sorted(P[l:r], key=lambda p: p.x)
         else:
             P[l:r] = sorted(P[l:r], key=lambda p: p.y)
-        T[t].location = mid
-        T[t].l = rec(l, mid, depth + 1)
-        T[t].r = rec(mid + 1, r, depth + 1)
 
-        return t
+        node = Node()
+        node.location = mid
+        node.l = rec(l, mid, depth + 1)
+        node.r = rec(mid + 1, r, depth + 1)
+
+        return node
 
     root = rec(0, len(P), 0)
-    return root, T
+    return root
 
 
-def find(P, T, root, sx, tx, sy, ty):
+def find(P, root, sx, tx, sy, ty):
     ans = []
 
-    def rec(v, depth):
-        nonlocal P, T, sx, tx, sy, ty, ans
+    def rec(node, depth):
+        nonlocal P, sx, tx, sy, ty, ans
 
-        x = P[T[v].location].x
-        y = P[T[v].location].y
+        x = P[node.location].x
+        y = P[node.location].y
 
         if sx <= x <= tx and sy <= y <= ty:
-            ans.append(P[T[v].location])
+            ans.append(P[node.location])
 
         if depth % 2 == 0:
-            if T[v].l is not None:
-                if sx <= x:
-                    rec(T[v].l, depth + 1)
-            if T[v].r is not None:
-                if x <= tx:
-                    rec(T[v].r, depth + 1)
+            if node.l is not None and sx <= x:
+                rec(node.l, depth + 1)
+            if node.r is not None and x <= tx:
+                rec(node.r, depth + 1)
         else:
-            if T[v].l is not None:
-                if sy <= y:
-                    rec(T[v].l, depth + 1)
-            if T[v].r is not None:
-                if y <= ty:
-                    rec(T[v].r, depth + 1)
+            if node.l is not None and sy <= y:
+                rec(node.l, depth + 1)
+            if node.r is not None and y <= ty:
+                rec(node.r, depth + 1)
 
     rec(root, 0)
     return ans
 
 
-def process_queries(P, T, root):
+def process_queries(P, root):
     q = int(stdin.readline())
     for i in range(q):
         sx, tx, sy, ty = [int(x) for x in stdin.readline().split()]
-        ans = find(P, T, root, sx, tx, sy, ty)
+        ans = find(P, root, sx, tx, sy, ty)
         [print(a.id) for a in sorted(ans)]
         print()
 
 
 def main():
     P = read_points()
-    root, T = make_kd_tree(P)
-    process_queries(P, T, root)
+    root = make_kd_tree(P)
+    process_queries(P, root)
 
 
 if __name__ == '__main__':
