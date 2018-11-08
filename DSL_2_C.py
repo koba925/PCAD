52d8ebb69de146f29687cf4285c6bad323d55528
Takahiro Kobayashi <koba925@gmail.com>
2018-11-08 18:57:39 +0900

    DSL_2_C: Removed global variables

--- DSL_2_C.py
+++ DSL_2_C.py
@@ -1,11 +1,10 @@
-import copy
-from sys import stdin, setrecursionlimit
+from sys import stdin
 
 
 class Node():
     def __init__(self):
         self.location = None
-        self.p = self.l = self.r = None
+        self.l = self.r = None
 
     def __repr__(self):
         return "Node({}, {}, {}, {}, {})".format(
@@ -13,8 +12,6 @@ class Node():
 
 
 class Point():
-    MAX = 1000000
-
     def __init__(self, id=None, x=None, y=None):
         self.id = id
         self.x = x
@@ -28,45 +25,44 @@ class Point():
             self.id, self.x, self.y)
 
 
-np = 0
-P = []
-T = []
-
-
 def read_points():
-    global P, T
-
     n = int(stdin.readline())
+    P = []
     for i in range(n):
         x, y = [int(x) for x in stdin.readline().split()]
         P.append(Point(i, x, y))
-        T.append(Node())
-    return n
+    return P
 
 
-def make_kd_tree(l, r, depth):
-    global np, P, T
+def make_kd_tree(P):
+    T = [Node() for _ in range(len(P))]
+    np = 0
 
-    if l >= r:
-        return None
+    def rec(l, r, depth):
+        nonlocal np
 
-    mid = (l + r) // 2
-    t = np
-    np += 1
+        if l >= r:
+            return None
 
-    if depth % 2 == 0:
-        P[l:r] = sorted(P[l:r], key=lambda p: p.x)
-    else:
-        P[l:r] = sorted(P[l:r], key=lambda p: p.y)
-    T[t].location = mid
-    T[t].l = make_kd_tree(l, mid, depth + 1)
-    T[t].r = make_kd_tree(mid + 1, r, depth + 1)
+        mid = (l + r) // 2
+        t = np
+        np += 1
 
-    return t
+        if depth % 2 == 0:
+            P[l:r] = sorted(P[l:r], key=lambda p: p.x)
+        else:
+            P[l:r] = sorted(P[l:r], key=lambda p: p.y)
+        T[t].location = mid
+        T[t].l = rec(l, mid, depth + 1)
+        T[t].r = rec(mid + 1, r, depth + 1)
+
+        return t
+
+    root = rec(0, len(P), 0)
+    return root, T
 
 
-def find(v, sx, tx, sy, ty, depth, ans):
-    global P, T
+def find(P, T, v, sx, tx, sy, ty, depth, ans):
 
     x = P[T[v].location].x
     y = P[T[v].location].y
@@ -77,36 +73,33 @@ def find(v, sx, tx, sy, ty, depth, ans):
     if depth % 2 == 0:
         if T[v].l is not None:
             if sx <= x:
-                find(T[v].l, sx, tx, sy, ty, depth + 1, ans)
+                find(P, T, T[v].l, sx, tx, sy, ty, depth + 1, ans)
         if T[v].r is not None:
             if x <= tx:
-                find(T[v].r, sx, tx, sy, ty, depth + 1, ans)
+                find(P, T, T[v].r, sx, tx, sy, ty, depth + 1, ans)
     else:
         if T[v].l is not None:
             if sy <= y:
-                find(T[v].l, sx, tx, sy, ty, depth + 1, ans)
+                find(P, T, T[v].l, sx, tx, sy, ty, depth + 1, ans)
         if T[v].r is not None:
             if y <= ty:
-                find(T[v].r, sx, tx, sy, ty, depth + 1, ans)
+                find(P, T, T[v].r, sx, tx, sy, ty, depth + 1, ans)
 
 
-def process_queries(root):
-    global P
-
+def process_queries(P, T, root):
     q = int(stdin.readline())
     for i in range(q):
         sx, tx, sy, ty = [int(x) for x in stdin.readline().split()]
         ans = []
-        find(root, sx, tx, sy, ty, 0, ans)
+        find(P, T, root, sx, tx, sy, ty, 0, ans)
         [print(a.id) for a in sorted(ans)]
         print()
 
 
 def main():
-    setrecursionlimit(100)
-    n = read_points()
-    root = make_kd_tree(0, n, 0)
-    process_queries(root)
+    P = read_points()
+    root, T = make_kd_tree(P)
+    process_queries(P, T, root)
 
 
 if __name__ == '__main__':
