1f664283412540d4b05d01e552d4e6d348f1d3d6
Takahiro Kobayashi <koba925@gmail.com>
2018-12-03 20:40:17 +0900

    GRL_5_A: Refactored

--- GRL_5_A.py
+++ GRL_5_A.py
@@ -1,13 +1,15 @@
+from collections import deque
+
+
 class Node():
     def __init__(self, key):
         self.key = key
-        self.indeg = 0
         self.passed = False
         self.adj = []
 
     def __repr__(self):
         return "Node({}, {}, {})".format(
-            self.key, self.indeg,
+            self.key,
             [(a.key, w) for a, w in self.adj])
 
 
@@ -16,46 +18,36 @@ def read_inputs():
     T = [Node(i) for i in range(n)]
     for _ in range(n - 1):
         s, t, w = [int(x) for x in input().split()]
-        T[s].indeg += 1
-        T[t].indeg += 1
         T[s].adj.append((T[t], w))
         T[t].adj.append((T[s], w))
     return T
 
 
 def furthest(T, s):
-
     for v in T:
         v.passed = False
+
     fur = -1
     fur_v = None
-    S = []
-    S.append((s, 0))
+    Q = deque()
+    Q.append((s, 0))
 
-    while S != []:
-        v, vr = S.pop()
-        v.passed = True
-        is_end = True
-        for a, w in v.adj:
-            if not a.passed:
-                S.append((a, vr + w))
-                is_end = False
-        if is_end and vr > fur:
+    while len(Q) > 0:
+        v, vr = Q.popleft()
+        if vr > fur:
             fur = vr
             fur_v = v
+        v.passed = True
+        for a, w in v.adj:
+            if not a.passed:
+                Q.append((a, vr + w))
 
     return fur, fur_v
 
 
 def tree_diameter(T):
-
-    if len(T) == 1:
-        return 0
-
-    s = [s for s in T if s.indeg == 1][0]
-    fur, s2 = furthest(T, s)
-    fur, fur_v = furthest(T, s2)
-
+    _, s = furthest(T, T[0])
+    fur, _ = furthest(T, s)
     return fur
 
 
