2be9c27b334d6fa968c56f14466e5d154d330934
Takahiro Kobayashi <koba925@gmail.com>
2019-08-15 12:21:27 +0900

    DPL_1_A: Solved

--- DPL_1_A.py
+++ DPL_1_A.py
@@ -1,21 +1,23 @@
 from typing import List
-from functools import lru_cache
 from sys import setrecursionlimit
 
 
 def changes(amount: int, kind: int, coins: List[int]) -> int:
-    
-    @lru_cache(maxsize=None)
+
+    cache: List[List[int]] = [[False] * (kind + 1) for _ in range(amount + 1)]
+
     def rec(a: int, k: int) -> int:
-        # print(a, k)
-        if a == 0:
-            return 0
-        elif a < 0 or k < 0:
+        if a < 0 or k < 0:
             return 50001
+        if cache[a][k]:
+            return cache[a][k]
+        if a == 0:
+            result = 0
         else:
-            return min(
-                rec(a - coins[k], k) + 1,
-                rec(a, k - 1))
+            result = min(rec(a - coins[k], k) + 1, rec(a, k - 1))
+
+        cache[a][k] = result
+        return result
 
     return rec(amount, kind - 1)
 
@@ -23,7 +25,6 @@ def changes(amount: int, kind: int, coins: List[int]) -> int:
 def main() -> None:
     n, m = [int(x) for x in input().split()]
     c = [int(x) for x in input().split()]
-    # print(n, m, c)
 
     setrecursionlimit(10000)
     print(changes(n, m, c))
